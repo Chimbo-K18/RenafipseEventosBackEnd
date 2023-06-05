d824c82ddbb84bdc3e6b4fe29e2fb66770aad657
Chimbo-K18 <kevin.chimbo.est@tecazuay.edu.ec>
2023-06-05 14:38:21 -0500

    first commit

---
+++ src/main/java/com/renafipse/eventos/BackProjectEventosRenafipse/BackProjectEventosRenafipseApplication.java
@@ -0,0 +1,13 @@
+package com.renafipse.eventos.BackProjectEventosRenafipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProjectEventosRenafipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProjectEventosRenafipseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/renafipse/eventos/BackProjectEventosRenafipse/BackProjectEventosRenafipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renafipse.eventos.BackProjectEventosRenafipse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProjectEventosRenafipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
